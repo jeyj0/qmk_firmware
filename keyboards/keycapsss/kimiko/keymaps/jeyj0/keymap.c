9631e96d5b332b22928fae08204dd97504aa36a5
Jannis Jorre <jannis@jorre.dev>
2021-06-21 16:05:33 +0200

    fix typo

--- keyboards/keycapsss/kimiko/keymaps/jeyj0/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/jeyj0/keymap.c
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
     KC_ESC,  KC_A,    KC_S,    KC_D,    SFT_KCF, KC_G,                        KC_H,    SFT_KCJ, KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     XXXXXXX, XXXXXXX,  KC_N,    KC_M,    COM_QUS, DOT_EXC, SLS_BSL, KC_RCTL,
-                      _______, _______, _______, ________, _______, _______,  _______, _______, _______, _______
+                      _______, _______, _______, _______,  _______, _______,  _______, _______, _______, _______
 ),
 
 [_NUM] = LAYOUT(
