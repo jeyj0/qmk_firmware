89ded93c71e1964d42d41e1272c82aa088ebc20d
Jannis Jorre <jannis@jorre.dev>
2021-06-21 16:02:55 +0200

    fix typo on options and increase tapping term

--- keyboards/keycapsss/kimiko/keymaps/jeyj0/config.h
+++ keyboards/keycapsss/kimiko/keymaps/jeyj0/config.h
@@ -49,5 +49,6 @@
 #define SPLIT_USB_DETECT
 #define NO_USB_STARTUP_CHECK
 
+#define TAPPING_TERM 500
 #define PERMISSIVE_HOLD // better mod-tap when typing fast
-#define IGNORE_MOD_TAG_INTERRUPT // make mod-tap be more strict when to act as mod
+#define IGNORE_MOD_TAP_INTERRUPT // make mod-tap be more strict when to act as mod
