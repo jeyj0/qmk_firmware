3c4f51c72e0e049731e05b9296dd096ef24d5ff9
Jannis Jorre <jannis@jorre.dev>
2023-08-17 08:29:53 +0200

    even more gaming layer changes, searching for the holy grail apparently

--- keyboards/keycapsss/kimiko/keymaps/jeyj0/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/jeyj0/keymap.c
@@ -139,10 +139,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_GAMING] = LAYOUT(
-    KC_ESC,  KC_5,    KC_6,    KC_7,    KC_8,    KC_9,                        KC_B,    _______, _______, KC_F1,   KC_F2,   KC_F3,
+    KC_ESC,  KC_5,    KC_6,    KC_7,    KC_8,    KC_9,                        _______, _______, _______, KC_F1,   KC_F2,   KC_F3,
     KC_TAB,  KC_LCTL, KC_Q,    KC_W,    KC_E,    KC_ESC,                      _______, KC_WH_U, KC_WH_D, KC_F4,   KC_F5,   KC_F6,
-    KC_T,    KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                        KC_H,    KC_BTN1, KC_BTN2, KC_F7,   KC_F8,   KC_F9,
-    KC_F3,   KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,     _______, _______,  _______, _______, _______, KC_F10,  KC_F11,  KC_F12,
+    KC_R,    KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                        KC_H,    KC_BTN1, KC_BTN2, KC_F7,   KC_F8,   KC_F9,
+    KC_T,    KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,     _______, _______,  KC_B,    _______, _______, KC_F10,  KC_F11,  KC_F12,
                       KC_LGUI, _______, KC_LCTL, KC_SPC,   KC_SPC,  _______,  _______, _______, _______, _______
 ),
 
